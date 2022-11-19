12f9007ae98dd8eb9467e52bb352ff862a394657
benie254 <davinci.monalissa@gmail.com>
2022-11-19 10:04:27 +0300

    update routing to fix issue

--- Enzi/src/app/modules/home/home-routing.module.ts
+++ Enzi/src/app/modules/home/home-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './components/landing/landing.component';
 import { HomeComponent } from './home.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [{ path: '', component: LandingComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
